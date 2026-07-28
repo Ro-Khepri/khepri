ff70af4ee19fd107b34a862e520e32bed7fa1898
Antoine Masson <antoine.masson@ecole-air.fr>
2026-07-28 13:14:53 +0200

    Premier import de 00-KHEPRI

---
+++ A-MANUEL/OPALE_KHEPRI/Generation/A-PUBLICATIONS/CADRAGE_AIDES/CADRAGE_AIDES_web.publi/auroraAW/lib-md/w_emeraudeMgr/emeraudeMgr.js
@@ -0,0 +1,18 @@
+/* === Emeraude template manager ============================================ */
+var emeraudeMgr = {
+
+/* === Public API =========================================================== */
+	/** init function - must be called at the end of page body */
+	init : function(){
+		try {
+			tplMgr.fStrings[0] = "Fermer";
+			tplMgr.fStrings[1] = "Cacher le menu de l\'activité";
+			tplMgr.fStrings[2] = "Menu";
+			tplMgr.fStrings[3] = "Afficher le menu de l\'activité";
+			if (scPaLib.checkNode(".module.emeraude", document.body)) tplMgr.fZenMode = 3;
+			else tplMgr.fZenMode = 2;
+		} catch(e) {
+			scCoLib.log("emeraudeMgr init failed: "+e);
+		}
+	}
+}
